82a029de4eb03e10c1075238065bf75c8a5a6a44
k38-suzuki <suzuki.kenta38@jaea.go.jp>
2025-03-07 08:19:51 +0900

    Add minio-client-plugin sample

---
+++ sample/minio-client-plugin/src/MinIOClient.h
@@ -0,0 +1,55 @@
+/**
+    @author Kenta Suzuki
+*/
+
+#ifndef CNOID_MINIO_CLIENT_PLUGIN_MINIO_CLIENT_H
+#define CNOID_MINIO_CLIENT_PLUGIN_MINIO_CLIENT_H
+
+#include <cnoid/Referenced>
+#include <cnoid/Signal>
+#include <QStringList>
+#include <vector>
+#include "exportdecl.h"
+
+namespace cnoid {
+
+class ExtensionManager;
+
+class CNOID_EXPORT MinIOClient : public Referenced
+{
+public:
+    static void initializeClass(ExtensionManager* ext);
+
+    MinIOClient();
+    MinIOClient(const QString& aliasName, const QString& bucketName);
+    virtual ~MinIOClient();
+
+    QString alias() const;
+    void setAlias(const QString& aliasName);
+    QString bucket() const;
+    void setBucket(const QString& bucketName);
+
+    void createBucket(const QString& bucketName);
+    void deleteBucket(const QString& bucketName);
+    void putObject(const QString& fileName, const QString& newPath);
+    void getObject(const QString& fileName, const QString& objectKey);
+    void deleteObject(const QString& objectKey);
+    void listObjects();
+
+    SignalProxy<void(std::string bucket_name)> sigBucketCreated();
+    SignalProxy<void(std::string bucket_name)> sigBucketDeleted();
+    SignalProxy<void(std::string object_name)> sigObjectUploaded();
+    SignalProxy<void(std::string object_name)> sigObjectDownloaded();
+    SignalProxy<void(std::string object_name)> sigObjectDeleted();
+    SignalProxy<void(std::vector<std::string> object_names)> sigObjectListed();
+
+private:
+    class Impl;
+    Impl* impl;
+};
+
+typedef ref_ptr<MinIOClient> MinIOClientPtr;
+
+}
+
+#endif // CNOID_MINIO_CLIENT_PLUGIN_MINIO_CLIENT_H
