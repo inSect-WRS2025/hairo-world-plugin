9efbe90b2110c9f55f01e198b85ab0c26dd7b585
k38-suzuki <suzuki.kenta38@jaea.go.jp>
2025-03-14 20:56:20 +0900

    Fix PD gain

--- sample/spot/sample/SpotLiedownController.cpp
+++ sample/spot/sample/SpotLiedownController.cpp
@@ -11,23 +11,23 @@ using namespace std;
 using namespace cnoid;
 
 const double down[] = {
-    20.0, 95.0, -143.0, -20.0, 95.0, -143.0,
-    20.0, 95.0, -143.0, -20.0, 95.0, -143.0
+    0.0, 96.2, -143.4, 0.0, 96.2, -143.4,
+    0.0, 96.2, -143.4, 0.0, 96.2, -143.4
 };
 
 const double up[] = {
-    25.0, 60.0, -95.0, -25.0, 60.0, -95.0,
-    25.0, 60.0, -95.0, -25.0, 60.0, -95.0
+    0.0, 60.4, -96.2, 0.0, 60.4, -96.2,
+    0.0, 60.4, -96.2, 0.0, 60.4, -96.2
 };
 
 class SpotLiedownController : public SimpleController
 {
     SimpleControllerIO* io;
-    int jointActuationMode;
-    Body* ioBody;
-    VectorXd qref, qold, qref_old;
-    double time;
-    double timeStep;
+    int legActuationMode;
+    Link* legJoint[12];
+    double qref[12];
+    double qprev[12];
+    double dt;
 
     SharedJoystickPtr joystick;
     int targetMode;
@@ -38,34 +38,31 @@ public:
     {
         this->io = io;
         ostream& os = io->os();
-        ioBody = io->body();
+        Body* body = io->body();
 
-        jointActuationMode = Link::JointVelocity;
+        legActuationMode = Link::JointEffort;
         for(auto opt : io->options()) {
             if(opt == "position") {
-                jointActuationMode = Link::JointDisplacement;
+                legActuationMode = Link::JointDisplacement;
                 os << "The joint-position command mode is used." << endl;
             } else if(opt == "velocity") {
-                jointActuationMode = Link::JointVelocity;
+                legActuationMode = Link::JointVelocity;
                 os << "The joint-velocity command mode is used." << endl;
             }
         }
 
-        const int nj = ioBody->numJoints();
-        qold.resize(nj);
-        for(int i = 0; i < nj; ++i) {
-            Link* joint = ioBody->joint(i);
-            joint->setActuationMode(jointActuationMode);
+        for(int i = 0; i < 12; ++i) {
+            Link* joint = legJoint[i] = body->joint(i);
+            if(!joint) {
+                os << "Turret joint " << i << " is not found." << endl;
+                return false;
+            }
+            qref[i] = qprev[i] = joint->q();
+            joint->setActuationMode(legActuationMode);
             io->enableIO(joint);
-            double q = joint->q();
-            qold[i] = q;
         }
 
-        qref = qold;
-        qref_old = qold;
-
-        time = 0.0;
-        timeStep = io->timeStep();
+        dt = io->timeStep();
 
         joystick = io->getOrCreateSharedObject<SharedJoystick>("joystick");
         targetMode = joystick->addMode();
@@ -77,36 +74,34 @@ public:
     {
         joystick->updateState(targetMode);
 
-        double pos[2];
-        for(int i = 0; i < 2; ++i) {
-            pos[i] = joystick->getPosition(targetMode,
-                i == 0 ? Joystick::L_TRIGGER_AXIS : Joystick::R_TRIGGER_AXIS);
-            if(fabs(pos[i]) < 0.2) {
-                pos[i] = 0.0;
+        static const double P = 150.0;
+        static const double D = 15.0;
+
+        for(int i = 0; i < 12; ++i) {
+            Link* joint = legJoint[i];
+            double pos = joystick->getPosition(
+                targetMode, Joystick::R_TRIGGER_AXIS);
+            if(fabs(pos) < 0.15) {
+                pos = 0.0;
+            }
+
+            double qe = pos > 0.0 ? radian(down[i]) : radian(up[i]);
+            double q = joint->q();
+            double dq = (q - qprev[i]) / dt;
+            double dqref = 0.0;
+            double deltaq = (qe - q) * dt;
+            qref[i] += deltaq;
+            dqref = deltaq / dt;
+
+            if(legActuationMode == Link::JointDisplacement) {
+                joint->q_target() = joint->q() + deltaq;
+            } else if(legActuationMode == Link::JointVelocity) {
+                joint->dq_target() = dqref;
+            } else if(legActuationMode == Link::JointEffort) {
+                joint->u() = P * (qref[i] - q) + D * (dqref - dq);
+                qprev[i] = q;
             }
         }
-
-        for(int i = 0; i < ioBody->numJoints(); ++i) {
-            double q = ioBody->joint(i)->q();
-            double q_target = pos[1] > 0 ? radian(down[i]) : radian(up[i]);
-            double deltaq = q_target - q;
-            qref[i] += deltaq * timeStep;
-        }
-
-        for(int i = 0; i < ioBody->numJoints(); ++i) {
-            Link* joint = ioBody->joint(i);
-            if(jointActuationMode == Link::JointDisplacement) {
-                joint->q_target() = qref[i];
-            } else if(jointActuationMode == Link::JointVelocity) {
-                double q = joint->q();
-                double dq = (q - qold[i]) / timeStep;
-                double dq_ref = (qref[i] - qref_old[i]) / timeStep;
-                joint->dq_target() = dq_ref;
-                qold[i] = q;
-            }
-        }
-        qref_old = qref;
-        time += timeStep;
 
         return true;
     }
